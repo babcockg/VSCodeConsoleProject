f549f5d54fa263fa7935267c1f2763b713f5975a
Galen Babcock <galen.babcock@gmail.com>
2019-09-15 11:03:59 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace C__Console_App
+{
+    class Program
+    {
+        int depth = 0;
+
+        public string spc ( int width )
+        {
+            return new string ( '.' , width*2 );
+        }
+
+        public void EnumerateDirectory(DirectoryInfo dir)
+        {
+            depth ++;
+            try
+            {
+                System.Console.WriteLine($"{spc(depth)}{dir.FullName}");
+
+                var files = dir.GetFiles();
+                foreach ( var file in files )
+                {
+                    System.Console.WriteLine($"{spc(depth+1)}{file.Name}");
+                }
+                var subDirectories = dir.GetDirectories();
+                foreach (var subDirectory in subDirectories)
+                {
+                    EnumerateDirectory(subDirectory);
+                }
+            }
+            catch 
+            {
+
+            }
+            depth --;
+        }
+
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.EnumerateDirectory(new DirectoryInfo(@"C:\"));
+        }
+    }
+}
